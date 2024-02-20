c9c91f42428c2d4000163701397bd513715f5864
walleffernandes <walleffernandesnascimento@outlook.com>
2024-02-20 16:53:19 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn lzkfilmes.wsgi --log-file -
---
+++ filme/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, redirect, reverse
+from .models import Filme, Usuario
+from .forms import CriarContaForm, FormHomepage
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+def homepage(request):
+    return render(request, "homepage.html")
+
+class Homepage(FormView):
+    template_name = "homepage.html"
+    form_class = FormHomepage
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+
+            return redirect('filme:homefilmes')
+        else:
+            return super().get(request, *args, **kwargs)
+
+    def get_success_url(self):
+            email = self.request.POST.get('email')
+            usuarios = Usuario.objects.filter(email=email)
+            if usuarios:
+                return reverse('filme:login')
+            else:
+                return reverse('filme:criarconta')
+
+
+class HomeFilmes(LoginRequiredMixin, ListView):
+    template_name = 'homefilmes.html'
+    model = Filme
+
+class Detalhesfilme(LoginRequiredMixin, DetailView):
+    template_name = 'detalhesfilme.html'
+    model = Filme
+
+    def get(self, request, *args, **kwargs):
+        filme = self.get_object()
+        filme.visualizacoes += 1
+        filme.save()
+        usuario = request.user
+        usuario.filmes_vistos.add(filme)
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(Detalhesfilme, self).get_context_data(**kwargs)
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)
+        context['filmes_relacionados'] = filmes_relacionados
+        return context
+
+class PesquisaFilme(LoginRequiredMixin, ListView):
+    template_name = 'pesquisa.html'
+    model = Filme
+
+    def get_queryset(self):
+        termo_pesquisa = self.request.GET.get('query')
+        if termo_pesquisa:
+            object_list = Filme.objects.filter(titulo__icontains=termo_pesquisa)
+            return object_list
+        else:
+            return  None
+
+
+
+class Paginaperfil(LoginRequiredMixin, UpdateView):
+    template_name = "editarperfil.html"
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
+
+class Criarconta(FormView):
+    template_name = "criarconta.html"
+    form_class = CriarContaForm
+
+    def get_success_url(self):
+
+        def form_valid(self, form):
+            form.save()
+            return super().form_valid(form)
+
+        return reverse('filme:login')
