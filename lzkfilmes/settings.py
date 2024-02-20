579f7f94ee4d6287717a0c28c94985a59e947486
walleffernandes <walleffernandesnascimento@outlook.com>
2024-02-20 17:00:52 -0300

    debug para falso

--- lzkfilmes/settings.py
+++ lzkfilmes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w(a_oekm&g^m=by)_k2x-1^@j#rx-h@2nts(@t#^!mrt#kpbu+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
